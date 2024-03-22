0f27186ae06c46018f9fedaf0542c9133ccb61cd
Canz <stephencarins@gmail.com>
2024-03-22 12:12:04 +1000

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 
-getent passwd retrouser > /dev/null
-if [ $? -ne 0 ]; then
-    # User does not exist
-    # Create a user for retroshare
-    mkdir /home/retrouser
-    useradd -s /bin/bash -d /home/retrouser -p "tmp" retrouser
-    PASSWD=`gpg --gen-random --armor 0 8`
-    echo "retrouser password: $PASSWD"
-    echo "retrouser:$PASSWD" | chpasswd
-    chown -R retrouser:retrouser /home/retrouser
-    chmod -R ug+rwX /home/retrouser
-    chmod -R o-rwx /home/retrouser
-    mkdir /run/user/1000
-    mkdir /run/user/1000/xpra
-    chown -R retrouser:retrouser /run/user/1000
-fi
-
 if [[ $MODE == "nogui" ]]
 then
 	su - retrouser -c "retroshare"
@@ -25,25 +8,9 @@ then
 	su - retrouser -c "retroshare-nogui --webinterface 9090 --docroot /usr/share/retroshare/webui/ --http-allow-all"
 elif [[ $MODE == "gui" ]]
 then
-	#xpra start :100
-
-        #ls /tmp/*
-        #tail /tmp/:100.log
-	
- 	#screen -dmS RetroScreen
-	#screen -S "RetroScreen" -p 0 -X stuff "DISPLAY=:100 RetroShare& $(printf \\r)"
-	
-        # QT_QPA_PLATFORM=xcb
 	su - retrouser -c "xpra start :100 --bind-tcp=0.0.0.0:14500 --no-mdns --no-notifications --no-pulseaudio"
-	# # start RetroShare GUI in a screen session with xpra display
-	sleep 5
-        su - retrouser -c "DISPLAY=:100 retroshare"
-
-	while true; do
-    		sleep 60  # Adjust the sleep time as needed
-	done
-
- 
+	sleep 2
+        su - retrouser -c "DISPLAY=:100 retroshare" 
 else
 	echo "Wrong mode selected"
 fi
