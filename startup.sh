890c803898ccf5d05d5fdea0cadead6b2db9e217
Canz <stephencarins@gmail.com>
2024-03-22 14:52:13 +1000

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,15 +2,15 @@
 
 if [[ $MODE == "nogui" ]]
 then
-	su - retrouser -c "retroshare"
+	retroshare
 elif [[ $MODE == "nogui-web" ]]
 then
-	su - retrouser -c "retroshare-nogui --webinterface 9090 --docroot /usr/share/retroshare/webui/ --http-allow-all"
+	retroshare-nogui --webinterface 9090 --docroot /usr/share/retroshare/webui/ --http-allow-all
 elif [[ $MODE == "gui" ]]
 then
-	su - retrouser -c "xpra start :100 --bind-tcp=0.0.0.0:14500 --no-mdns --no-notifications --no-pulseaudio"
+	xpra start :100 --bind-tcp=0.0.0.0:14500 --no-mdns --no-notifications --no-pulseaudio
 	sleep 2
-        su - retrouser -c "DISPLAY=:100 retroshare" 
+        DISPLAY=:100 retroshare
 else
 	echo "Wrong mode selected"
 fi
